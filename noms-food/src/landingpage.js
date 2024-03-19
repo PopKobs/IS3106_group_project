e71e5637169f7855786a3c5078a08b21d3fad1bd
shannontyx <shannon18yx@gmail.com>
2024-03-19 15:20:25 +0800

    index js edits

---
+++ noms-food/src/landingpage.js
@@ -0,0 +1,46 @@
+import db from "./firebase";
+import logo from './logo.svg';
+import './signup.css';
+
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  query,
+  where,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+
+const LandingPage = () => {
+    const history = useHistory();
+
+    const handleSignupLink = () => {
+        history.push("/signup");
+    }
+    const handleCreateListing = () => {
+        history.push("/createListing");
+    }
+    
+
+  return (
+    <>
+      <div>
+        <button onClick={handleSignupLink}>
+            Sign up
+        </button>
+      </div>
+
+      <div>
+        <button onClick={handleCreateListing}>
+            Create Listing Page
+        </button>
+      </div>
+     
+    </>
+  );
+};
+
+export default LandingPage;
