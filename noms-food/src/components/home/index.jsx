fd5269fd339f2f6dd6a50a49469d7d35ab91db25
displeasedcat <senindanial@gmail.com>
2024-03-26 11:35:36 +0800

    css fix

--- noms-food/src/components/home/index.jsx
+++ noms-food/src/components/home/index.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useAuth } from '../../contexts/authContext';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import '../../signup.css';
 
 const Home = () => {
     const { currentUser } = useAuth();
     const navigate = useNavigate(); // Initialize useNavigate
 
     return (
+        <div className='container'>
         <div className='text-2xl font-bold pt-14'>
             Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
-            <button
+            
+        </div>
+        <div>
+        <button
                 onClick={() => navigate('/createlisting')} // Use navigate to go to the Create Listing page
                 className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
                 Go to Create Listing
             </button>
+        </div>
         </div>
     );
 };
