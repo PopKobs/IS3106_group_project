1fd9bfc848f866762f018dbc82303b2807224fed
displeasedcat <senindanial@gmail.com>
2024-04-16 17:12:05 +0800

    Fixed Dumb Google

--- noms-food/src/vendor/ViewStore.js
+++ noms-food/src/vendor/ViewStore.js
@@ -13,6 +13,7 @@ import { useAuth } from '../contexts/authContext'; // Adjust the import path as 
 import { LoadScript, GoogleMap, Marker } from '@react-google-maps/api'; // Correctly imported
 import { useNavigate } from 'react-router-dom';
 import './ViewStore.css';
+import { Box, Container, Typography, TextField, Stack, Button } from "@mui/material";
 
 function ViewStore() {
     const { currentUserEmail } = useAuth();
@@ -42,7 +43,9 @@ function ViewStore() {
                 const storeDoc = await getDoc(storeRef);
 
                 if (storeDoc.exists()) {
-                    setStore(storeDoc.data());
+                    const storeData = storeDoc.data()
+                    setStore(storeData);
+                    console.log(storeDoc);
                 } else {
                     console.log("No store found.");
                 }
@@ -72,24 +75,23 @@ function ViewStore() {
     }, [store, isMapsApiLoaded]); // Depend on store.location and isMapsApiLoaded
     
 
-    if (!store) {
-        return <div>Loading store information...</div>;
-    }
+    // if (!store) {
+    //     return <div>Loading store information...</div>;
+    // }
 
     const containerStyle = {
         width: '800px', // Adjust width as needed
         height: '400px' // Adjust height as needed
     };
 
-    console.log(store.location);
+    //console.log(store.location);
 
     return (
-        <LoadScript
-            googleMapsApiKey="AIzaSyAPomcsuwYqpr_xLpQPAfZOFI3AxxuldJs" 
-            onLoad={() => setIsMapsApiLoaded(true)} 
-        >
-            <div className="ViewStore-container">
-                <br/> <br/>
+        <Container>
+            {!store ? <div>Loading store information...</div>
+            :
+            <Box>
+            <Box>
                 <h2 className="ViewStore-header">View Your Store</h2>
                 <div className="ViewStore-details">
                 <p><b>Store Name:</b> {store.name}</p>
@@ -98,6 +100,12 @@ function ViewStore() {
                 <p><b>Closing Hours:</b> {store.closing}</p>
                 <p><b>Store Location:</b> {address}</p>
                 </div>
+            </Box>
+            <LoadScript
+            googleMapsApiKey="AIzaSyAPomcsuwYqpr_xLpQPAfZOFI3AxxuldJs" 
+            onLoad={() => setIsMapsApiLoaded(true)} 
+        >
+            <div className="ViewStore-container">
                 <div className="ViewStore-mapContainer">
                     <GoogleMap
                         mapContainerStyle={containerStyle}
@@ -112,6 +120,10 @@ function ViewStore() {
                 <button className="ViewStore-button" onClick={handleEditStore}>Edit Store</button> <br/><br/>
             </div>
         </LoadScript>
+        </Box>
+            }
+        
+        </Container>
     );
 }
 
